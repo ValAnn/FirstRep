b9cf54a2a882b9f2a7bdd8ef2b03e3928465b611
Анна Валова <valovaanna04@mail.ru>
2023-09-05 21:37:15 +0400

    Тестовый код 2

--- firstCode/firstCode/Form1.cs
+++ firstCode/firstCode/Form1.cs
@@ -19,7 +19,7 @@ namespace firstCode
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("прив:))");
+            MessageBox.Show("прив:)) ааааааааааааааааааааа");
 
         }
     }
