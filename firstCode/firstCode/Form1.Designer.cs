01ef86f6cf91ba497d3c310c30a36e410ed5a6d1
Анна Валова <valovaanna04@mail.ru>
2023-09-05 21:36:40 +0400

    Тестовый код

--- firstCode/firstCode/Form1.Designer.cs
+++ firstCode/firstCode/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(237, 182);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(253, 96);
+            this.button.TabIndex = 0;
+            this.button.Text = "Нажми меня!!";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button;
     }
 }
 
