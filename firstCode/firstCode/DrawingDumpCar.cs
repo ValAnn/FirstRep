ad836d3a3e4888d1210c8b116e165254e2d92f93
Анна Валова <valovaanna04@mail.ru>
2023-09-08 15:43:22 +0400

    Пример с гоночной машиной

---
+++ firstCode/firstCode/DrawingDumpCar.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace firstCode
+{
+    
+    public class DrawningDumpTruck
+    {
+        
+        public DumpTruck EntityDumpTruck { get; private set; }
+
+        private int _pictureWidth;
+
+        private int _pictureHeight;
+        
+        private int _startPosX;
+
+        private int _startPosY;
+
+        private readonly int _carWidth = 110;
+
+        private readonly int _carHeight = 60;
+
+        public bool Init(int speed, double weight, Color bodyColor, Color additionalColor, bool bodyKit, bool wing, bool sportLine, int width, int height)
+        {
+            _pictureWidth = width;
+            _pictureHeight = height;
+            EntityDumpTruck = new DumpTruck();
+            EntityDumpTruck.Init(speed, weight, bodyColor, additionalColor, bodyKit, wing, sportLine);
+            return true;
+        }
+       
+        public void SetPosition(int x, int y)
+        {
+            // TODO: Изменение x, y
+            _startPosX = x;
+            _startPosY = y;
+        }
+
+        public void MoveTransport(DirectionType direction)
+        {
+            if (EntityDumpTruck == null)
+            {
+                
+                return;
+            }
+            switch (direction)
+            {
+                //влево
+                case DirectionType.Left:
+                    if (_startPosX - EntityDumpTruck.Step > 0)
+                    {
+                        _startPosX -= (int)EntityDumpTruck.Step;
+                    }
+                    break;
+                //вверх
+                case DirectionType.Up:
+                   
+                    if (_startPosY - EntityDumpTruck.Step > 0)
+                    {
+                        _startPosY -= (int)EntityDumpTruck.Step;
+                    }
+                    break;
+                // вправо
+                case DirectionType.Right:
+                    if (_startPosX + EntityDumpTruck.Step + _carWidth < _pictureWidth)
+                    {
+                        _startPosX += (int)EntityDumpTruck.Step;
+                        
+                    }
+                    break;
+                //вниз
+                case DirectionType.Down:
+                    
+                    if (_startPosY + EntityDumpTruck.Step + _carHeight < _pictureHeight)
+                    {
+                        _startPosY += (int)EntityDumpTruck.Step;
+                    }
+                    break;
+            }
+        }
+        
+        public void DrawTransport(Graphics g)
+        {
+            if (EntityDumpTruck == null)
+            {
+                return;
+            }
+            
+            Pen pen = new Pen(Color.Black);
+            Brush additionalBrush = new
+            SolidBrush(EntityDumpTruck.AdditionalColor);
+            // обвесы
+            if (EntityDumpTruck.BodyKit)
+            {
+                g.DrawEllipse(pen, _startPosX + 90, _startPosY, 20, 20);
+                g.DrawEllipse(pen, _startPosX + 90, _startPosY + 40, 20,
+                20);
+                g.DrawRectangle(pen, _startPosX + 90, _startPosY + 10,
+                20, 40);
+                g.DrawRectangle(pen, _startPosX + 90, _startPosY, 15,
+                15);
+                g.DrawRectangle(pen, _startPosX + 90, _startPosY + 45,
+                15, 15);
+                g.FillEllipse(additionalBrush, _startPosX + 90,
+                _startPosY, 20, 20);
+                g.FillEllipse(additionalBrush, _startPosX + 90,
+                _startPosY + 40, 20, 20);
+                g.FillRectangle(additionalBrush, _startPosX + 90,
+                _startPosY + 10, 20, 40);
+            g.FillRectangle(additionalBrush, _startPosX + 90,
+            _startPosY + 1, 15, 15);
+                g.FillRectangle(additionalBrush, _startPosX + 90,
+                _startPosY + 45, 15, 15);
+                g.DrawEllipse(pen, _startPosX, _startPosY, 20, 20);
+                g.DrawEllipse(pen, _startPosX, _startPosY + 40, 20, 20);
+                g.DrawRectangle(pen, _startPosX, _startPosY + 10, 20,
+                40);
+                g.DrawRectangle(pen, _startPosX + 5, _startPosY, 14,
+                15);
+                g.DrawRectangle(pen, _startPosX + 5, _startPosY + 45,
+                14, 15);
+                g.FillEllipse(additionalBrush, _startPosX, _startPosY,
+                20, 20);
+                g.FillEllipse(additionalBrush, _startPosX, _startPosY +
+                40, 20, 20);
+                g.FillRectangle(additionalBrush, _startPosX + 1,
+                _startPosY + 10, 25, 40);
+                g.FillRectangle(additionalBrush, _startPosX + 5,
+                _startPosY + 1, 15, 15);
+                g.FillRectangle(additionalBrush, _startPosX + 5,
+                _startPosY + 45, 15, 15);
+                g.DrawRectangle(pen, _startPosX + 35, _startPosY, 39,
+                15);
+                g.DrawRectangle(pen, _startPosX + 35, _startPosY + 45,
+                39, 15);
+                g.FillRectangle(additionalBrush, _startPosX + 35,
+                _startPosY + 1, 40, 15);
+                g.FillRectangle(additionalBrush, _startPosX + 35,
+                _startPosY + 45, 40, 15);
+            }
+            //границы автомобиля
+            
+            g.DrawEllipse(pen, _startPosX + 10, _startPosY + 5, 20, 20);
+            g.DrawEllipse(pen, _startPosX + 10, _startPosY + 35, 20, 20);
+            g.DrawEllipse(pen, _startPosX + 80, _startPosY + 5, 20, 20);
+            g.DrawEllipse(pen, _startPosX + 80, _startPosY + 35, 20, 20);
+            g.DrawRectangle(pen, _startPosX + 9, _startPosY + 15, 10, 30);
+            g.DrawRectangle(pen, _startPosX + 90, _startPosY + 15, 10,
+            30);
+            g.DrawRectangle(pen, _startPosX + 20, _startPosY + 4, 70, 52);
+            //задние фары
+            Brush brRed = new SolidBrush(Color.Red);
+            g.FillEllipse(brRed, _startPosX + 10, _startPosY + 5, 20, 20);
+            g.FillEllipse(brRed, _startPosX + 10, _startPosY + 35, 20,
+            20);
+            //передние фары
+            Brush brYellow = new SolidBrush(Color.Yellow);
+            g.FillEllipse(brYellow, _startPosX + 80, _startPosY + 5, 20,
+            20);
+            g.FillEllipse(brYellow, _startPosX + 80, _startPosY + 35, 20,
+            20);
+            //кузов
+            Brush br = new SolidBrush(EntityDumpTruck.BodyColor);
+            g.FillRectangle(br, _startPosX + 10, _startPosY + 15, 10, 30);
+            g.FillRectangle(br, _startPosX + 90, _startPosY + 15, 10, 30);
+            g.FillRectangle(br, _startPosX + 20, _startPosY + 5, 70, 50);
+            
+        //стекла
+Brush brBlue = new SolidBrush(Color.LightBlue);
+            g.FillRectangle(brBlue, _startPosX + 70, _startPosY + 10, 5,
+            40);
+            g.FillRectangle(brBlue, _startPosX + 30, _startPosY + 10, 5,
+            40);
+            g.FillRectangle(brBlue, _startPosX + 35, _startPosY + 8, 35,
+            2);
+            g.FillRectangle(brBlue, _startPosX + 35, _startPosY + 51, 35,
+            2);
+            //выделяем рамкой крышу
+            g.DrawRectangle(pen, _startPosX + 35, _startPosY + 10, 35,
+            40);
+            g.DrawRectangle(pen, _startPosX + 75, _startPosY + 15, 25,
+            30);
+            g.DrawRectangle(pen, _startPosX + 10, _startPosY + 15, 15,
+            30);
+            // спортивная линия
+            if (EntityDumpTruck.SportLine)
+            {
+                g.FillRectangle(additionalBrush, _startPosX + 75,
+                _startPosY + 23, 25, 15);
+                g.FillRectangle(additionalBrush, _startPosX + 35,
+                _startPosY + 23, 35, 15);
+                g.FillRectangle(additionalBrush, _startPosX + 10,
+                _startPosY + 23, 20, 15);
+            }
+            // крыло
+            if (EntityDumpTruck.Wing)
+            {
+                g.FillRectangle(additionalBrush, _startPosX, _startPosY
+                + 5, 10, 50);
+                g.DrawRectangle(pen, _startPosX, _startPosY + 5, 10,
+                50);
+            }
+        }
+    }
+}
+
+
--- firstCode/firstCode/Form1.Designer.cs
+++ firstCode/firstCode/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace firstCode
 {
-    partial class Form1
+    partial class DumpTruckForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,34 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
             this.button = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(884, 461);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button
             // 
-            this.button.Location = new System.Drawing.Point(237, 182);
+            this.button.Location = new System.Drawing.Point(315, 392);
             this.button.Name = "button";
-            this.button.Size = new System.Drawing.Size(253, 96);
-            this.button.TabIndex = 0;
-            this.button.Text = "Нажми меня!!";
+            this.button.Size = new System.Drawing.Size(202, 36);
+            this.button.TabIndex = 2;
+            this.button.Text = "Добавить";
             this.button.UseVisualStyleBackColor = true;
-            this.button.Click += new System.EventHandler(this.button1_Click);
+            this.button.Click += new System.EventHandler(this.button_Click);
             // 
-            // Form1
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackColor = System.Drawing.Color.Transparent;
+            this.buttonUp.BackgroundImage = global::firstCode.Properties.Resources.up;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(721, 308);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(59, 57);
+            this.buttonUp.TabIndex = 3;
+            this.buttonUp.UseVisualStyleBackColor = false;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.BackgroundImage = global::firstCode.Properties.Resources.right;
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(786, 371);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(59, 57);
+            this.buttonRight.TabIndex = 4;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.BackgroundImage = global::firstCode.Properties.Resources.down;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(721, 371);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(59, 57);
+            this.buttonDown.TabIndex = 5;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.BackgroundImage = global::firstCode.Properties.Resources.left;
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(656, 371);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(59, 57);
+            this.buttonLeft.TabIndex = 6;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // DumpTruckForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonUp);
             this.Controls.Add(this.button);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox);
+            this.Name = "DumpTruckForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DumpTruck";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonLeft;
     }
 }
 
